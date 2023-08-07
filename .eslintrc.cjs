83fba7d20fec1e2e9683e90ff86e9d139b144a33
wudongdong0921 <1159543738@qq.com>
2023-08-07 16:08:27 +0800

    ci(elint): add elint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
 /*
  * @Author: web东东 wudongdong@wintopgis.com
  * @Date: 2023-08-03 10:47:13
- * @LastEditors: web东东 wudongdong@wintopgis.com
- * @LastEditTime: 2023-08-07 13:15:40
+ * @LastEditors: wudongdong0921 1159543738@qq.com
+ * @LastEditTime: 2023-08-07 15:56:52
  * @FilePath: \textAutomate\.eslintrc.cjs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,7 +17,7 @@ module.exports = {
     parserOptions: {
       ecmaVersion: 12,
       sourceType: 'module',
-      project: './tsconfig.json'
+      project: './tsconfig.eslint.json'
     },
     plugins: ['@typescript-eslint', 'prettier'],
     rules: {}
